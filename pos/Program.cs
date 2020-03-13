e9e4b1d4a3bfc8d6f113891344d2782cf6c44f7d
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-03-13 17:43:19 +0530

    removed license issue from TSMultiComboBox

--- pos/Program.cs
+++ pos/Program.cs
@@ -1,23 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 
 using System.Windows.Forms;
 
 namespace pos
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
+
             Application.Run(new login());
         }
-
     }
 }
