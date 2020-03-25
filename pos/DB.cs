9644e56d1f0675ef0f1d21099de0dadfe47ca524
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-03-26 01:50:40 +0530

    signin certificate removed

--- pos/DB.cs
+++ pos/DB.cs
@@ -23,7 +23,7 @@ namespace pos
             try
             {
                 conn = new SqlConnection();
-                readfromAddress = File.ReadAllLines("D:\\configSettings.txt");
+                readfromAddress = File.ReadAllLines("C:\\conf\\configSettings.txt");
                 conn.ConnectionString = "User ID='" + readfromAddress[4].Split('/')[0] + "';Password='" + readfromAddress[4].Split('/')[1] + "';data source='" + readfromAddress[4].Split('/')[2] + "';" + "persist security info=true;initial catalog='" + readfromAddress[4].Split('/')[3] + "';Connect Timeout=30";
                 // conn.ConnectionString = "User ID='" + readfromAddress[0].Split('/')[0] + "';Password='" + readfromAddress[0].Split('/')[1] + "';data source='" + readfromAddress[0].Split('/')[2]+ "';" + "persist security info=true;initial catalog='" + readfromAddress[0].Split('/')[3] + "';Connect Timeout=0";
 
