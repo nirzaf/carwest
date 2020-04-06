2a01a2ef1ca21517ebd3dc79e8d9c838aa1dec54
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-06 22:35:17 +0530

    SMS Sender Optimized

--- pos/SMSSender.cs
+++ pos/SMSSender.cs
@@ -12,9 +12,15 @@ namespace pos
         public static void SendWebRequest(string DNumber, string Message)
         {
             string request = "https://bulksms2.etisalat.lk/sendsmsmultimask.php?";
-            DNumber = "94" + DNumber;
+            if (DNumber.StartsWith("0"))
+            {
+                DNumber = DNumber.Remove(0, 1);
+            }
+            if (!DNumber.StartsWith("94"))
+            {
+                DNumber = "94" + DNumber;
+            }
             var postData = "USER=" + HttpUtility.UrlPathEncode(username) + "&PWD=" + HttpUtility.UrlPathEncode(password) + "&MASK=" + HttpUtility.UrlPathEncode(callerId) + "&NUM=" + HttpUtility.UrlPathEncode(DNumber) + "&MSG=" + HttpUtility.UrlDecode(Message);
-
             using (var web = new WebClient())
             {
                 Debug.WriteLine(request + postData);
